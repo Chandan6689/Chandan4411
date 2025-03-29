9580a69eb244ad3c4b7c5e888996f90257553309
Chandan6689 <tempttuku@gmail.com>
2025-03-29 16:32:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title> CH-CHEAT </title>
+    <link rel="stylesheet" href="assets/stylegg.css">
+</head>
+
+<body>
+    <canvas id="canvas" width="1400" height="600">
+</canvas>
+    <script src="assets/script.js">
+    </script>
+</body>
+
+</html>
